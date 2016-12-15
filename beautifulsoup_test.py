642a98421fe0c87af631cee9724bccc33b5b2504
DarkSand <darkwings_love@163.com>
2016-12-15 11:55:51 +0800

    python learn

---
+++ beautifulsoup_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import requests
+from bs4 import BeautifulSoup as bsp
+import re
+
+response = requests.get("http://www.jd.com", verify=False)
+soup = bsp(response.content)
+
+
+def number_span(tag):
+    return tag.name == 'a' and '登录' in tag.text
+
+
+print soup.find(number_span)
